91da57bf6860fac851633e56415434aa1891bdea
Filip Troníček <filip.tronicek@seznam.cz>
2020-02-12 16:15:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: "https://related-pics.now.sh/"
+      origin: "https://related-pics.now.sh"
     })
   );
 app.get("/", function(req, res) {
